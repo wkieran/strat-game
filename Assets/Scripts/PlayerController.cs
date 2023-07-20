990cca3bafc073265f32973e971f021f28a1a25d
wkieran <kieranwhitsitt@outlook.com>
2023-07-20 14:47:36 -0400

    added walking animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,10 @@ public class PlayerController : MonoBehaviour
 
     public LayerMask whatStopMovement;
 
+    public Animator animator;
 
+    Vector2 movement;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,20 +23,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f) { //could also say <= 0f
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, whatStopMovement)) {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+
+            if (Mathf.Abs(movement.x) == 1f) {
+
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(movement.x, 0f, 0f), 0.2f, whatStopMovement)) {
+                    
+                    movePoint.position += new Vector3(movement.x, 0f, 0f);
+                    animator.SetFloat("Vertical", 0f);
+                    animator.SetFloat("Horizontal", movement.x);
 
                 }
-            } else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, whatStopMovement)) {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            } else if (Mathf.Abs(movement.y) == 1f) {
 
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, movement.y, 0f), 0.2f, whatStopMovement)) {
+                    Debug.Log("gang shit");
+
+                    movePoint.position += new Vector3(0f, movement.y, 0f);
+                    animator.SetFloat("Horizontal", 0f);
+                    animator.SetFloat("Vertical", movement.y);
                 }
+
             }
+            animator.SetFloat("Speed", movement.sqrMagnitude);
         }
+
+        //animator.SetFloat("Speed", 0f);
+
     }
 }
